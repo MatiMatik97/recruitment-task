d4685c4c02725a890b4338eb76e7c53e39ac9ee7
MatiMatik97 <matimatik97@gmail.com>
2023-08-11 09:29:43 +0200

    003 - fetch data hook

---
+++ src/hooks/use-fetch-data.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useFetchData = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState({ error: false, message: '' });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+                setData(response.data);
+            } catch (error) {
+                setError({ error: true, message: error?.message || 'Error fetching data' });
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return {
+        data,
+        loading,
+        error
+    };
+};
+
+export default useFetchData;
